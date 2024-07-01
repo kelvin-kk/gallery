857827f38252dfc97e1722fdd83c3e32c768b516
kelvin-kk <kelvinkiariek@gmail.com>
2024-07-01 15:31:20 +0300

    Updated connection string to _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://kelvin:kiarie09@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://kelvin:kiarie09@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://kelvin:kiarie09@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://kelvin:kiarie09@moringagallery.3dbzfuq.mongodb.net/?retryWrites=true&w=majority&appName=MoringaGallery',
+    development: 'mongodb+srv://kelvin:kiarie09@moringagallery.3dbzfuq.mongodb.net/?retryWrites=true&w=majority&appName=MoringaGallery',
+    test: 'mongodb+srv://kelvin:kiarie09@moringagallery.3dbzfuq.mongodb.net/?retryWrites=true&w=majority&appName=MoringaGallery',
 }
 module.exports = config;
